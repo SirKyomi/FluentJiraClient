f0f87d3eaedf453473e4caf69bb37048437dda85
Elias-Constantin Regber <eliasstieler@icloud.com>
2024-07-16 14:58:58 +0200

    Better Builder, some Code changes

--- FluentJiraClient/JiraClient.cs
+++ FluentJiraClient/JiraClient.cs
@@ -9,19 +9,38 @@ public class JiraClient : IResponseMappingStage {
     private readonly HttpClient _httpClient;
     private string _path;
 
-    public JiraClient(string url, string username, string password) {
+    #region Constructors
+
+    public JiraClient(string url, string authMethod, string username, string password) {
         _httpClient = new HttpClient() {
             BaseAddress = new Uri(Path.Combine(url, "rest/api/latest/")),
             DefaultRequestHeaders = {
-                Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(Encoding.ASCII.GetBytes($"{username}:{password}")))
+                Authorization = new AuthenticationHeaderValue(authMethod, Convert.ToBase64String(Encoding.ASCII.GetBytes($"{username}:{password}")))
             }
         };
     }
+
+    public JiraClient(string url, string authMethod, string accessToken) {
+        _httpClient = new HttpClient() {
+            BaseAddress = new Uri(Path.Combine(url, "rest/api/latest/")),
+            DefaultRequestHeaders = {
+                Authorization = new AuthenticationHeaderValue(authMethod, accessToken)
+            }
+        };
+    }
+
+    #endregion
+    
+    #region Get
 
     public IResponseMappingStage Get(string path) {
         _path = path;
         return this;
     }
+
+    #endregion
+    
+    #region Mapping
 
     public async Task<T> AsAsync<T>() {
         var response = await GetAndEnsureSuccessStatusCodeAsync();
@@ -40,6 +59,8 @@ public class JiraClient : IResponseMappingStage {
         response.EnsureSuccessStatusCode();
         return response;
     }
+
+    #endregion
 }
 
 public interface IResponseMappingStage {
--- FluentJiraClient/JiraClientBuilder.cs
+++ FluentJiraClient/JiraClientBuilder.cs
@@ -4,6 +4,8 @@ public class JiraClientBuilder : IUrlSelectionStage, IUsernameSelectionStage, IP
     private string _url;
     private string _username;
     private string _password;
+    private string _onlyAccessToken;
+    private string _authMethod = "Basic";
 
     public static IUrlSelectionStage Create() {
         return new JiraClientBuilder();
@@ -24,6 +26,11 @@ public class JiraClientBuilder : IUrlSelectionStage, IUsernameSelectionStage, IP
         return this;
     }
 
+    public IBuildStage UsingOnlyAccessToken(string accessToken) {
+        _onlyAccessToken = accessToken;
+        return this;
+    }
+
     public IBuildStage WithPassword(string password) {
         _password = password;
         return this;
@@ -35,8 +42,21 @@ public class JiraClientBuilder : IUrlSelectionStage, IUsernameSelectionStage, IP
         return this;
     }
 
+    public IBuildStage UsingAuthMethod(string authMethod) {
+        _authMethod = authMethod;
+        return this;
+    } 
+    public IBuildStage UsingBearerAuthentication() {
+        UsingAuthMethod("Bearer");
+        return this;
+    } 
+    public IBuildStage UsingBasicAuthentication() {
+        UsingAuthMethod("Basic");
+        return this;
+    }
+
     public JiraClient Build() {
-        return new JiraClient(_url, _username, _password);
+        return _onlyAccessToken == "Bearer" ? new JiraClient(_url,_authMethod, _onlyAccessToken) : new JiraClient(_url,_authMethod, _username, _password);
     }
 }
 
@@ -47,6 +67,7 @@ public interface IUrlSelectionStage {
 public interface IUsernameSelectionStage {
     IPasswordSelectionStage WithUsername(string username);
     IPasswordSelectionStage WithEmail(string email);
+    IBuildStage UsingOnlyAccessToken(string accessToken);
 }
 
 public interface IPasswordSelectionStage {
@@ -55,5 +76,8 @@ public interface IPasswordSelectionStage {
 }
 
 public interface IBuildStage {
+    IBuildStage UsingAuthMethod(string authMethod);
+    IBuildStage UsingBearerAuthentication();
+    IBuildStage UsingBasicAuthentication();
     JiraClient Build();
 }
